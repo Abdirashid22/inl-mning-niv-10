eb5abbcb9257e08def81dde25264c59cff09dfbb
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-17 14:22:27 +0100

    Add project files.

---
+++ inlämning nivå 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int jämnaTal = 0; 
+
+        Console.WriteLine("Skriv in 10 heltal:");
+
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Ange tal {i}: ");
+            int tal = int.Parse(Console.ReadLine());
+
+            
+            if (tal % 2 == 0)
+            {
+                jämnaTal++; 
+            }
+        }
+
+        
+        Console.WriteLine($"Antalet jämna tal du skrev in är: {jämnaTal}");
+    }
+}
+
